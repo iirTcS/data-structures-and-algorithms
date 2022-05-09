a12d76994f52c95073b32cb088e4c42027958ce6
Corneliu Rotari <corneliu.rotari99@gmail.com>
2022-05-09 16:27:14 +0300

    Done: lab 10/Graf

--- Lab/lab10/Graph.c
+++ Lab/lab10/Graph.c
@@ -9,35 +9,87 @@ TGraphL* createGraphAdjList(int numberOfNodes) {
 	graph->nn = numberOfNodes;
 	graph->adl = malloc(sizeof(ATNode) * numberOfNodes);
 	for (int i = 0; i < numberOfNodes; i++) {
-		graph->adl[i]->next = NULL;
+		// graph->adl[i] = malloc(sizeof(TNode));
+		graph->adl[i] = NULL;
 	}
 	return graph;
 }
 
 void addEdgeList(TGraphL* graph, int v1, int v2) {
-	//TODO: 1
+	if (graph) {
+			ATNode new1 = malloc(sizeof(TNode));
+			new1->next = graph->adl[v1];
+			new1->v = v2;
+			graph->adl[v1] = new1;
+
+			
+			ATNode new = malloc(sizeof(TNode));
+			new->next = graph->adl[v2];
+			new->v = v1;
+			graph->adl[v2] = new;
+	}
 }
 
 void dfsRecHelper(TGraphL* graph, int* visited, List* path, int s) {
-	//TODO: 2
+	// Visited node
+	visited[s] = 1;
+	enqueue(path, s);
+
+	// Every neighbour
+	for (TNode* aux = graph->adl[s]; aux!= NULL; aux = aux->next) {
+		if (visited[aux->v] == 0) {
+			dfsRecHelper(graph, visited, path, aux->v);
+		}
+	}
 }
 
 List* dfsRecursive(TGraphL* graph, int s) {
-	// TODO 2
-	return NULL;
+	List *path = createList();
+	int *visited = calloc(sizeof(int), graph->nn);
+	dfsRecHelper(graph, visited, path, s);
+	free(visited);
+	return path;
 }
 
-List* bfs(TGraphL* graph, int s){
-	// TODO: 3
-	return NULL;
+List* bfs(TGraphL* graph, int s) {
+	Queue *q = createQueue();
+	List *l = createList();
+	int *visited = calloc(sizeof(int), graph->nn);
+	enqueue(q, s);
+	enqueue(l, s);
+	visited[s] = 1;
+
+	while (!isQueueEmpty(q)) {
+		int v = front(q);
+		dequeue(q);
+		for (TNode *aux = graph->adl[v]; aux != NULL; aux = aux->next) {
+			if (visited[aux->v] == 0) {
+				visited[aux->v] = 1;
+				enqueue(l,aux->v);
+				enqueue(q, aux->v);
+			}
+		}
+	}
+	destroyQueue(q);
+	free(visited);
+	return l;
 }
 
 
-void destroyGraphAdjList(TGraphL* graph){
-	// TODO: 4
+void destroyGraphAdjList(TGraphL* graph) {
+	for (int i = 0; i < graph->nn; i++) {
+		TNode* it = graph->adl[i]; 
+		while (it != NULL) {
+			TNode* aux = it;
+			it = it->next;
+			free(aux);
+		}
+	}
+	free(graph->adl);
+	free(graph);
 }
 
-void removeEdgeList(TGraphL* graph, int v1, int v2){
+void removeEdgeList(TGraphL* graph, int v1, int v2) {
 	TNode* it = graph->adl[v1];
 	TNode* prev = NULL;
 	while(it != NULL && it->v != v2){
@@ -68,7 +120,7 @@ void removeEdgeList(TGraphL* graph, int v1, int v2){
 	free(it);
 }
 
-void removeNodeList(TGraphL* graph, int v){
+void removeNodeList(TGraphL* graph, int v) {
 	for(int i = 0; i < graph->nn;i++){
 		removeEdgeList(graph,v,i);
 	}
